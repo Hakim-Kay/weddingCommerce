82535a05580638ce24cedf584fad833ed4ffebe2
Makini Hakim Kayira <95077113+Hakim-Kay@users.noreply.github.com>
2025-03-02 19:39:03 +0000

    [SubIssue/WPP-002] Simple Tailwindcss and Nuxt UI integration (#7)
    * [SubIssue/WPP-002] Simple Tailwindcss and Nuxt UI integration
    
    ---------
    
    Co-authored-by: MakiniKayiraGDS <makini.kayira@digital.cabinet-office.gov.uk>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,8 @@ export default defineNuxtConfig({
 
   modules: [
     '@pinia/nuxt',
+    '@nuxt/ui',
+    '@nuxtjs/tailwindcss',
   ],
 
   css: [
@@ -43,5 +45,29 @@ export default defineNuxtConfig({
     }
   },
 
-  compatibilityDate: '2025-03-01'
+  compatibilityDate: '2025-03-01',
+
+  // UI Configuration
+  ui: {
+    global: true
+  },
+
+  // Tailwind Configuration
+  tailwindcss: {
+    exposeConfig: true,
+    viewer: true,
+    config: {
+      content: [
+        './components/**/*.{js,vue,ts}',
+        './layouts/**/*.vue',
+        './pages/**/*.vue',
+        './plugins/**/*.{js,ts}',
+        './app.vue',
+        './error.vue'
+      ],
+      theme: {
+        extend: {}
+      }
+    }
+  },
 })
